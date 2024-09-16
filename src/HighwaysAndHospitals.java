5ee674820321bc935d4d58f7e565d9e9f23b4188
diegovillegas <diego.villegas@menloschool.org>
2024-09-16 10:36:08 -0700

    commit #3

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -15,53 +15,59 @@ public class HighwaysAndHospitals {
      *  hospital access for all citizens in Menlo County.
      */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
-        int minCost = 0;
-        // Idea: Find which cities have a city in common and build a hospital in that city and have the other cities connect to it
-        int sortedCities[][] = new int[cities.length][2];
-        // Separate cities in order to see which ones are connected and which are not
-        // Find most common city
-        int costOnlyHospitals = n * hospitalCost;
+        // Checks if it is cheaper to buy hospitals in every city
+        if (hospitalCost < highwayCost) {
+            return hospitalCost * n;
+        }
+        long minCost = 0;
+        int rootOne = 0;
+        int rootTwo = 0;
+        // Array of cities where index = node and value = root
+        int edgesAndRoots[] = new int[n + 1];
+        // Set all edges equal to eachother
+        for(int i = 1; i < edgesAndRoots.length; i++){
+            edgesAndRoots[i] = i;
+        }
+        // For each node
         for(int i = 0; i < cities.length; i++){
-            for(int j = 0; j < cities[0].length; j++){
-                sortedCities[i][j] = cities[i][j];
+            // Check root of left city
+            rootOne = edgesAndRoots[cities[i][0]];
+            // Check root of right city
+            rootTwo = edgesAndRoots[cities[i][1]];
+            // Checks if we need to check the roots of the rootOne
+            if(rootOne != edgesAndRoots[cities[i][1]]){
+                rootOne = getFinalRoot(edgesAndRoots[rootOne], rootOne, edgesAndRoots);
+            }
+            // Checks if we need to check the roots of the rootTwo
+            if(rootTwo != edgesAndRoots[cities[i][1]]){
+                rootTwo = getFinalRoot(edgesAndRoots[rootTwo], rootTwo, edgesAndRoots);
+            }
+            // If roots are not the same, make 1st city root of 2nd city
+            if(rootOne != rootTwo){
+                edgesAndRoots[cities[i][1]] = cities[i][0];
             }
         }
-        int temp = 0;
-        int tempTwo = 0;
-        // sort the cities from smallest to largest using the first element
-        // Bubble sort (can probably change this later for a more efficient method
-        for(int i = 1; i < cities.length; i++){
-            for(int j = 0; j < cities.length - i; j++){
-                if(sortedCities[j][0] > sortedCities[j + 1][0]){
-                    // Swap
-                    temp = sortedCities[j][0];
-                    tempTwo = sortedCities[j][1];
-                    sortedCities[j][0] = sortedCities[j + 1][0];
-                    sortedCities[j][1] = sortedCities[j + 1][1];
-                    sortedCities[j + 1][0] = temp;
-                    sortedCities[j + 1][1] = tempTwo;
-                }
+        int counter = 0;
+        // Counts sub-graphs by counting which nodes are equal to themselves
+        for(int i = 1; i < edgesAndRoots.length; i++){
+            System.out.println("node: " + i + ", root: " + edgesAndRoots[i]);
+            if(edgesAndRoots[i] == i){
+                counter++;
             }
         }
-        // Cut of the array into sections
-        // Situation for when there is only 2 cities
-        if(n == 2){
-            minCost = highwayCost + hospitalCost;
-            if(minCost > costOnlyHospitals){
-                return costOnlyHospitals;
-            }
-        }
-        // Debugging
-        for(int i = 0; i < sortedCities.length; i++){
-            System.out.println(sortedCities[i][0] + ", " + sortedCities[i][1]);
-        }
-        // We can probably use some sort of sorting algorithm to do this
-        // Also im assuming some these have so that it might be cheaper to just build a hospital in every city so I have to figure out how to do that
-        // I think it would also be beneficial if we can somehow separate the cities because we know some of them are not connected
-        // After we figure out the cheapest way to build using highways, we compare to if we were to only use cities to see if it is cheaper
-        if(minCost > costOnlyHospitals){
-            return costOnlyHospitals;
-        }
+
+        System.out.println(counter);
+        minCost = (counter * hospitalCost) + (highwayCost * (n - counter));
         return minCost;
     }
+    private static int getFinalRoot(int root, int city, int edgesAndRoots[]){
+        int rootCopy = root;
+        int cityCopy = city;
+        while(rootCopy != cityCopy){
+            cityCopy = rootCopy;
+            rootCopy = edgesAndRoots[cityCopy];
+        }
+        return root;
+    }
 }
+
