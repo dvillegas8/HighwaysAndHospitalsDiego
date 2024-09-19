5041fc6c32f2460539f4e18c079dfaaca83a5e86
diegovillegas <diego.villegas@menloschool.org>
2024-09-18 18:59:17 -0700

    commit #4

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -9,65 +9,72 @@
  */
 
 public class HighwaysAndHospitals {
-
-    /**
-     * TODO: Complete this function, cost(), to return the minimum cost to provide
-     *  hospital access for all citizens in Menlo County.
-     */
+    // Returns the minimum cost to provide hospital access for all citizens in Menlo County.
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
         // Checks if it is cheaper to buy hospitals in every city
         if (hospitalCost < highwayCost) {
-            return hospitalCost * n;
+            return (long) hospitalCost * n;
         }
         long minCost = 0;
-        int rootOne = 0;
-        int rootTwo = 0;
+        int cityX = 0;
+        int cityY = 0;
+        int temp = 0;
+        int cityB = 0;
         // Array of cities where index = node and value = root
         int edgesAndRoots[] = new int[n + 1];
-        // Set all edges equal to eachother
-        for(int i = 1; i < edgesAndRoots.length; i++){
-            edgesAndRoots[i] = i;
-        }
-        // For each node
+        // For each edge
         for(int i = 0; i < cities.length; i++){
-            // Check root of left city
-            rootOne = edgesAndRoots[cities[i][0]];
-            // Check root of right city
-            rootTwo = edgesAndRoots[cities[i][1]];
-            // Checks if we need to check the roots of the rootOne
-            if(rootOne != edgesAndRoots[cities[i][1]]){
-                rootOne = getFinalRoot(edgesAndRoots[rootOne], rootOne, edgesAndRoots);
+            // Get left city
+            cityX = cities[i][0];
+            // Find top root/city of left city
+            while(edgesAndRoots[cityX] > 0){
+                cityX = edgesAndRoots[cityX];
             }
-            // Checks if we need to check the roots of the rootTwo
-            if(rootTwo != edgesAndRoots[cities[i][1]]){
-                rootTwo = getFinalRoot(edgesAndRoots[rootTwo], rootTwo, edgesAndRoots);
+            // Get right city
+            cityY = cities[i][1];
+            // Find top root/city of right city
+            while(edgesAndRoots[cityY] > 0){
+                cityY = edgesAndRoots[cityY];
             }
-            // If roots are not the same, make 1st city root of 2nd city
-            if(rootOne != rootTwo){
-                edgesAndRoots[cities[i][1]] = cities[i][0];
+            // Path compression
+            while(edgesAndRoots[cities[i][0]] > 0){
+                temp = edgesAndRoots[cities[i][0]];
+                edgesAndRoots[cities[i][0]] = cityX;
+                cities[i][0] = temp;
+            }
+            while(edgesAndRoots[cities[i][1]] > 0){
+                temp = edgesAndRoots[cities[i][1]];
+                edgesAndRoots[cities[i][1]] = cityY;
+                cities[i][1] = temp;
+            }
+            // If cities are not the same, do weight balancing
+            if(cityX != cityY){
+                // Weight balancing
+                if(edgesAndRoots[cityY] < edgesAndRoots[cityX]){
+                    // Update length of right city
+                    edgesAndRoots[cityY] = edgesAndRoots[cityY] + edgesAndRoots[cityX] - 1;
+                    // Set the left city's root at the right city
+                    edgesAndRoots[cityX] = cityY;
+                }
+                else{
+                    // Update length of left city
+                    edgesAndRoots[cityX] = edgesAndRoots[cityX] + edgesAndRoots[cityY] - 1;
+                    // Set the right city's root at the left city
+                    edgesAndRoots[cityY] = cityX;
+                }
             }
         }
         int counter = 0;
         // Counts sub-graphs by counting which nodes are equal to themselves
         for(int i = 1; i < edgesAndRoots.length; i++){
-            System.out.println("node: " + i + ", root: " + edgesAndRoots[i]);
-            if(edgesAndRoots[i] == i){
+            //System.out.println("node: " + i + ", root: " + edgesAndRoots[i]);
+            if(edgesAndRoots[i] <= 0){
                 counter++;
             }
         }
-
-        System.out.println(counter);
-        minCost = (counter * hospitalCost) + (highwayCost * (n - counter));
+        //System.out.println(counter);
+        minCost = ((long) counter * hospitalCost) + ((long) highwayCost * (n - counter));
         return minCost;
-    }
-    private static int getFinalRoot(int root, int city, int edgesAndRoots[]){
-        int rootCopy = root;
-        int cityCopy = city;
-        while(rootCopy != cityCopy){
-            cityCopy = rootCopy;
-            rootCopy = edgesAndRoots[cityCopy];
-        }
-        return root;
     }
 }
 
